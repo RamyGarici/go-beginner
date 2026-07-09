4b6bac3299bce289e77ab567dc42a9594ed70d3b
RamyGarici <ramymanilgarici@gmail.com>
2026-07-09 15:53:14 +0100

    base switch case

--- cli-to-do-list/main.go
+++ cli-to-do-list/main.go
@@ -1,6 +1,7 @@
 package main
 import ("time"
-"encoding/json")
+"fmt"
+"os")
 
 type Task struct{
     ID int `json:"id"`
@@ -12,6 +13,32 @@ type Task struct{
 
 
 func main(){
+for{
+	var i int
+	fmt.Println("Welcome to your ToDo list")
+	fmt.Println("Enter 1 to create a task")
+	fmt.Println("Enter 2 to list tasks")
+	fmt.Println("Enter 3 to delete a task")
+	fmt.Println("Enter 4 to mark a finished task")
+	fmt.Println("Enter 5 to stop the program")
+	fmt.Scan(&i)
+    switch i {
+	    case 1:
+			fmt.Println("Enter your task informations")
+		case 2:
+			fmt.Println("Task list")
+		case 3:
+			fmt.Println("Which task do you want to delete?")
+		case 4:
+			fmt.Println("Which task do you want to mark as finished?")
+		case 5:
+			fmt.Println("Thank you!")
+			os.Exit(0)
+		default:
+			fmt.Println("Wrong option")
 
+	}
+
+}
 
 }
