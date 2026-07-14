b1cca652fc586fe309915ca36475cf12d076d5ef
RamyGarici <ramymanilgarici@gmail.com>
2026-07-14 16:58:01 +0100

    adding workers

--- basic-port-scanner/main.go
+++ basic-port-scanner/main.go
@@ -8,15 +8,25 @@ import(
 
  var wg sync.WaitGroup
 func main(){
-   
-	
-	for i:=1;i<=100;i++{
-	    wg.Add(1)
-		go scanPort(i)
+	ports := make(chan int, 100)
+
+for i:=1;i<=100;i++{
+	    go worker(ports, &wg)
 	}
+   
+    
+	
+	for i:=1;i<=65535;i++{
+	    wg.Add(1)
+		ports <-i
+	}
+	
+	close(ports)
+	
 	wg.Wait()
 	
 }
+
 
 func scanPort(port int){
 addr := fmt.Sprintf("scanme.nmap.org:%v",port)
@@ -25,5 +35,13 @@ conn,err := net.DialTimeout("tcp",addr,2*time.Second)
 		fmt.Printf("The port %v is open\n",port)
 		conn.Close()
 	}
-	wg.Done()
+	
+}
+
+func worker(ports <-chan int, wg *sync.WaitGroup){
+	for port := range ports{
+		scanPort(port)
+		wg.Done()
+		
+	}
 }
