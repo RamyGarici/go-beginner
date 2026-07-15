06d3c5d0024caf286c02bc14fa8ec8a08bdac8ea
RamyGarici <ramymanilgarici@gmail.com>
2026-07-15 13:09:30 +0100

    encoding and decoding files

--- basic-file-encryption/main.go
+++ basic-file-encryption/main.go
@@ -1,21 +1,45 @@
 package main
+import("os"
+"fmt")
 
 
 
 func main(){
+	key := []byte("secretKey")
+	// Encoding:
+	file, err := os.ReadFile("secret.txt")
+	if err!=nil{
+		fmt.Printf("Error:%v\n",err)
+		os.Exit(0)
+
+	}
+	encryptedFile := xorEncoding(key,file)
+	os.WriteFile("secret.txt.enc", encryptedFile,0644)
+
+	//Decoding:
+	file, err = os.ReadFile("secret.txt.enc")
+	if err!=nil{
+		fmt.Printf("Error:%v\n",err)
+		os.Exit(0)
+
+	}
+	decryptedFile := xorEncoding(key,file)
+	os.WriteFile("secret.txt", decryptedFile,0644)
+
 
 }
 
 
 func xorEncoding(key []byte, data []byte)[]byte{
-	encryptedResult := make([]byte,0)
+	encryptedResult := make([]byte,len(data))
 	var xoredValue byte 
 	for i:=0;i<len(data);i++{
 		xoredValue = data[i] ^ key[i%len(key)]
-		encryptedResult = append(encryptedResult, xoredValue)
+		encryptedResult[i] = xoredValue
 	}
 
 
 
    return encryptedResult
 }
+
